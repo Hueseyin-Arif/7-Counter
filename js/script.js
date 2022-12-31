544eb35b4ae188aaeb18c9cac2ab86d781767036
Hueseyin_Arif <yogyakartali2017@gmail.com>
2022-12-31 20:04:44 +0100

    0-10 arasi sayma ve mobil uyumluluk eklendi

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function increase(){
+    var number;
+    number = document.getElementById('count').value;
+
+    if(number<11)
+    number= parseInt(number) +1;
+
+    if(number==11)
+    number=0
+
+    number = document.getElementById('count').value = number;
+}
+
+function decrease(){
+    var number;
+    number = document.getElementById('count').value;
+    if(number>-1)
+    number= parseInt(number) -1;
+
+    if(number==-1)
+    number=10
+
+    number = document.getElementById('count').value = number;
+}
+
